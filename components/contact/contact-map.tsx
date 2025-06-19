1cdbea889bbe7c876a66c86ca388e50361a4621f
adarsh-cmtai <adarsh.shahi@completrix.com>
2025-06-19 12:42:35 +0530

    Update contact-map.tsx

--- components/contact/contact-map.tsx
+++ components/contact/contact-map.tsx
@@ -34,7 +34,7 @@ export default function ContactMap() {
             <CardContent className="p-0">
               <div className="aspect-video w-full">
                 <iframe
-                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14024.908771231607!2d77.01853770781249!3d28.46985!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390d18a1d576c0cd%3A0x713f5cb4a4837e7e!2sSector%2014%2C%20Gurugram%2C%20Haryana%2C%20India!5e0!3m2!1sen!2sus!4v1715301000000!5m2!1sen!2sus"
+                  src="https://maps.app.goo.gl/BgF92meVaAmqerJV6"
                   width="100%"
                   height="500"
                   style={{ border: 0 }}
